23796539f0afe1c1ae315541ba7299834c6a0ea4
izumin5210 <masayuki@izumin.info>
2016-05-13 10:10:10 +0900

    Generate OAuthActivity

---
+++ app/src/main/java/info/izumin/android/sunazuri/presentation/activity/oauth/OAuthActivity.java
@@ -0,0 +1,14 @@
+package info.izumin.android.sunazuri.presentation.activity.oauth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import info.izumin.android.sunazuri.R;
+
+public class OAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oauth);
+    }
+}
