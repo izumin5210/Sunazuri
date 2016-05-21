3c7873d64bc24d3e27ffbe783eb69a8ed73eb2d7
izumin5210 <masayuki@izumin.info>
2016-05-21 17:28:58 +0900

    Start OAuthActivity after clicked btn-login

--- app/src/main/java/info/izumin/android/sunazuri/presentation/fragment/WelcomeFragment.java
+++ app/src/main/java/info/izumin/android/sunazuri/presentation/fragment/WelcomeFragment.java
@@ -1,24 +1,33 @@
 package info.izumin.android.sunazuri.presentation.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import info.izumin.android.sunazuri.R;
+import info.izumin.android.sunazuri.RequestCode;
+import info.izumin.android.sunazuri.databinding.FragmentWelcomeBinding;
+import info.izumin.android.sunazuri.presentation.activity.oauth.OauthActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WelcomeFragment extends Fragment {
+    public static final String TAG = WelcomeFragment.class.getSimpleName();
 
+    public interface Handler {
+        void onBtnLoginClick(View view);
+    }
 
     public WelcomeFragment() {
         // Required empty public constructor
     }
 
+    private FragmentWelcomeBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,4 +36,17 @@ public class WelcomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_welcome, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentWelcomeBinding.bind(view);
+        binding.setHandler(handler);
+    }
+
+    private final Handler handler = new Handler() {
+        @Override
+        public void onBtnLoginClick(View view) {
+            startActivityForResult(new Intent(getActivity(), OauthActivity.class), RequestCode.OAUTH.code);
+        }
+    };
 }
