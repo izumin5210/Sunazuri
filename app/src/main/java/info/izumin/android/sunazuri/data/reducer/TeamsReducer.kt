729e80d4ef8f41418b63dc41bcbf04df54efc779
izumin5210 <masayuki@izumin.info>
2016-05-22 19:33:56 +0900

    Add TeamsReducer to RootStore

---
+++ app/src/main/java/info/izumin/android/sunazuri/data/reducer/TeamsReducer.kt
@@ -0,0 +1,11 @@
+package info.izumin.android.sunazuri.data.reducer;
+
+import info.izumin.android.droidux.annotation.Reducer
+import info.izumin.android.sunazuri.domain.model.Teams
+
+/**
+ * Created by izumin on 5/22/2016 AD.
+ */
+@Reducer(Teams::class)
+class TeamsReducer {
+}
