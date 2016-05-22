d1d16b2bebe20b193b4bd58ba254ac8c7a72112f
izumin5210 <masayuki@izumin.info>
2016-05-22 18:29:12 +0900

    Add base action class

---
+++ app/src/main/java/info/izumin/android/sunazuri/data/action/common/BaseAsyncAction.java
@@ -0,0 +1,9 @@
+package info.izumin.android.sunazuri.data.action.common;
+
+import info.izumin.android.droidux.thunk.AsyncAction;
+
+/**
+ * Created by izumin on 5/22/2016 AD.
+ */
+public abstract class BaseAsyncAction<R extends BaseAction.RequestValue> extends BaseAction<R> implements AsyncAction {
+}
