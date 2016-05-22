b22c185f6ddb144559121b9806272c5b201bd7ad
izumin5210 <masayuki@izumin.info>
2016-05-22 19:54:04 +0900

    Implement TeamsRepository::get(AuthorizedUser)

--- app/src/main/java/info/izumin/android/sunazuri/infrastructure/repository/TeamsRepositoryImpl.java
+++ app/src/main/java/info/izumin/android/sunazuri/infrastructure/repository/TeamsRepositoryImpl.java
@@ -3,6 +3,8 @@ package info.izumin.android.sunazuri.infrastructure.repository;
 import info.izumin.android.sunazuri.domain.entity.AuthorizedUser;
 import info.izumin.android.sunazuri.domain.entity.Team;
 import info.izumin.android.sunazuri.domain.repository.TeamsRepository;
+import info.izumin.android.sunazuri.infrastructure.entity.mapper.AuthorizedUserMapper;
+import info.izumin.android.sunazuri.infrastructure.entity.mapper.TeamMapper;
 import info.izumin.android.sunazuri.infrastructure.repository.source.team.TeamsDataSourceFactory;
 import rx.Single;
 
@@ -16,14 +18,22 @@ class TeamsRepositoryImpl implements TeamsRepository {
     public static final String TAG = TeamsRepositoryImpl.class.getSimpleName();
 
     private final TeamsDataSourceFactory dataSourceFactory;
+    private final AuthorizedUserMapper authorizedUserMapper;
+    private final TeamMapper teamMapper;
 
     @Inject
-    TeamsRepositoryImpl(TeamsDataSourceFactory dataSourceFactory) {
+    TeamsRepositoryImpl(TeamsDataSourceFactory dataSourceFactory,
+                        AuthorizedUserMapper authorizedUserMapper,
+                        TeamMapper teamMapper) {
         this.dataSourceFactory = dataSourceFactory;
+        this.authorizedUserMapper = authorizedUserMapper;
+        this.teamMapper = teamMapper;
     }
 
     @Override
     public Single<List<Team>> get(AuthorizedUser user) {
-        return null;
+        return dataSourceFactory.createRemoteDataSource()
+                .getTeams(authorizedUserMapper.transform(user))
+                .map(teamMapper::transformFromTeamStatsEntities);
     }
 }
