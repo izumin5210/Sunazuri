5ca82fe092560fced96ec4d1f77a78fb4e39afd0
izumin5210 <masayuki@izumin.info>
2016-05-23 11:11:17 +0900

    Add navigation drawer to MainActivity

--- app/src/main/java/info/izumin/android/sunazuri/presentation/activity/MainActivity.java
+++ app/src/main/java/info/izumin/android/sunazuri/presentation/activity/MainActivity.java
@@ -1,17 +1,28 @@
 package info.izumin.android.sunazuri.presentation.activity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import info.izumin.android.sunazuri.R;
+import info.izumin.android.sunazuri.databinding.ActivityMainBinding;
 import info.izumin.android.sunazuri.presentation.fragment.WelcomeFragment;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setSupportActionBar(binding.toolbar);
+        final ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(this, binding.drawer, binding.toolbar, R.string.open, R.string.close);
+        binding.drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
