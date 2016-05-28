c58ceb60d18791feef4e28ba606dac3c43111d02
izumin5210 <masayuki@izumin.info>
2016-05-28 17:10:51 +0900

    RepositoryModule includes other repository modules

--- app/src/main/java/info/izumin/android/sunazuri/infrastructure/repository/RepositoryModule.java
+++ app/src/main/java/info/izumin/android/sunazuri/infrastructure/repository/RepositoryModule.java
@@ -2,30 +2,23 @@ package info.izumin.android.sunazuri.infrastructure.repository;
 
 import dagger.Module;
 import dagger.Provides;
-import info.izumin.android.sunazuri.domain.repository.OauthRepository;
-import info.izumin.android.sunazuri.domain.repository.TeamsRepository;
 import info.izumin.android.sunazuri.domain.repository.UsersRepository;
+import info.izumin.android.sunazuri.infrastructure.repository.source.oauth.OauthRepositoryModule;
+import info.izumin.android.sunazuri.infrastructure.repository.source.team.TeamsRepositoryModule;
 
 import javax.inject.Singleton;
 
 /**
  * Created by izumin on 5/13/2016 AD.
  */
-@Module
+@Module(
+        includes = {
+                OauthRepositoryModule.class,
+                TeamsRepositoryModule.class
+        }
+)
 public class RepositoryModule {
     public static final String TAG = RepositoryModule.class.getSimpleName();
-
-    @Provides
-    @Singleton
-    TeamsRepository teamsRepository(TeamsRepositoryImpl repo) {
-        return repo;
-    }
-
-    @Provides
-    @Singleton
-    OauthRepository oauthRepository(OauthRepositoryImpl repo) {
-        return repo;
-    }
 
     @Provides
     @Singleton
