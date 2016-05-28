315441ff0d0e7c60df56f6eeddfa65132319d259
izumin5210 <masayuki@izumin.info>
2016-05-28 15:42:38 +0900

    Use DataFactory and add getInstance() to companion object for mock apis

--- app/src/test/kotlin/info/izumin/android/sunazuri/infrastructure/mock/api/MockOauthApi.kt
+++ app/src/test/kotlin/info/izumin/android/sunazuri/infrastructure/mock/api/MockOauthApi.kt
@@ -2,7 +2,9 @@ package info.izumin.android.sunazuri.infrastructure.mock.api
 
 import info.izumin.android.sunazuri.infrastructure.api.OauthApi
 import info.izumin.android.sunazuri.infrastructure.entity.AccessTokenEntity
+import info.izumin.android.sunazuri.infrastructure.mock.DataFactory
 import retrofit2.mock.BehaviorDelegate
+import retrofit2.mock.MockRetrofit
 import rx.Completable
 import rx.Single
 
@@ -10,16 +12,18 @@ import rx.Single
  * Created by izumin on 5/21/2016 AD.
  */
 class MockOauthApi(val delegate: BehaviorDelegate<OauthApi>) : OauthApi {
-    val TOKEN = AccessTokenEntity()
 
-    init {
-        TOKEN.accessToken   = "testtoken"
-        TOKEN.tokenType     = "bearer"
-        TOKEN.scope         = "read+write"
+    companion object {
+        fun getInstance(mockRetrofit: MockRetrofit): MockOauthApi {
+            return MockOauthApi(mockRetrofit.create(OauthApi::class.java))
+        }
     }
 
+    var token = DataFactory.createAccessTokenEntity()
+        get set
+
     override fun fetchToken(clientId: String?, clientSecret: String?, grantType: String?, redirectUri: String?, code: String?): Single<AccessTokenEntity>? {
-        return delegate.returningResponse(TOKEN).fetchToken(clientId, clientSecret, grantType, redirectUri, code)
+        return delegate.returningResponse(token).fetchToken(clientId, clientSecret, grantType, redirectUri, code)
     }
 
     override fun revoke(authorization: String?): Completable? {
--- app/src/test/kotlin/info/izumin/android/sunazuri/infrastructure/mock/api/MockUsersApi.kt
+++ app/src/test/kotlin/info/izumin/android/sunazuri/infrastructure/mock/api/MockUsersApi.kt
@@ -2,29 +2,26 @@ package info.izumin.android.sunazuri.infrastructure.mock.api
 
 import info.izumin.android.sunazuri.infrastructure.api.UsersApi
 import info.izumin.android.sunazuri.infrastructure.entity.AuthorizedUserEntity
+import info.izumin.android.sunazuri.infrastructure.mock.DataFactory
 import retrofit2.mock.BehaviorDelegate
+import retrofit2.mock.MockRetrofit
 import rx.Single
-import java.util.*
 
 /**
  * Created by izumin on 5/21/2016 AD.
  */
 class MockUsersApi(val delegate: BehaviorDelegate<UsersApi>) : UsersApi {
 
-    val USER = AuthorizedUserEntity()
-    val USER_ID: Long = 1
-
-    init {
-        USER.id         = USER_ID
-        USER.name       = "test user"
-        USER.screenName = "test_user"
-        USER.createdAt  = Date()
-        USER.updatedAt  = Date()
-        USER.email      = "test@example.com"
-        USER.icon       = "http://example.com/test_user.png"
+    companion object {
+        fun getInstance(mockRetrofit: MockRetrofit): MockUsersApi {
+            return MockUsersApi(mockRetrofit.create(UsersApi::class.java))
+        }
     }
 
+    var user = DataFactory.createAuthorizedUserEntity()
+        get set
+
     override fun fetch(accessToken: String?): Single<AuthorizedUserEntity>? {
-        return delegate.returningResponse(USER).fetch(accessToken)
+        return delegate.returningResponse(user).fetch(accessToken)
     }
 }
