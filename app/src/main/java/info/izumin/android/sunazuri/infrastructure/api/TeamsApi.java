3cdbeb559e51bfc2f25f4abfadcd183f9732a381
izumin5210 <masayuki@izumin.info>
2016-06-10 22:09:09 +0900

    Add TeamsApi#categories

--- app/src/main/java/info/izumin/android/sunazuri/infrastructure/api/TeamsApi.java
+++ app/src/main/java/info/izumin/android/sunazuri/infrastructure/api/TeamsApi.java
@@ -1,10 +1,13 @@
 package info.izumin.android.sunazuri.infrastructure.api;
 
+import com.github.gfx.static_gson.annotation.JsonSerializable;
+import com.google.gson.FieldNamingPolicy;
 import info.izumin.android.sunazuri.infrastructure.entity.TeamEntity;
 import info.izumin.android.sunazuri.infrastructure.entity.TeamStatsEntity;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+import rx.Observable;
 import rx.Single;
 
 import java.util.List;
@@ -29,4 +32,17 @@ public interface TeamsApi {
             @Path("team_name") String teamName,
             @Query("access_token") String accessToken
     );
+
+    @GET("/v1/teams/{team_name}/categories")
+    Observable<Category> categories(
+            @Path("team_name") String teamName,
+            @Query("access_token") String accessToken
+    );
+
+    @JsonSerializable(fieldNamingPolicy = FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+    class Category {
+        public String name;
+        public int count;
+        public List<Category> children;
+    }
 }
--- app/src/test/kotlin/info/izumin/android/sunazuri/infrastructure/mock/api/MockTeamsApi.kt
+++ app/src/test/kotlin/info/izumin/android/sunazuri/infrastructure/mock/api/MockTeamsApi.kt
@@ -6,13 +6,13 @@ import info.izumin.android.sunazuri.infrastructure.entity.TeamStatsEntity
 import info.izumin.android.sunazuri.infrastructure.mock.DataFactory
 import retrofit2.mock.BehaviorDelegate
 import retrofit2.mock.MockRetrofit
+import rx.Observable
 import rx.Single
 
 /**
  * Created by izumin on 5/21/2016 AD.
  */
 class MockTeamsApi(val delegate: BehaviorDelegate<TeamsApi>) : TeamsApi {
-
     companion object {
         fun getInstance(mockRetrofit: MockRetrofit): MockTeamsApi {
             return MockTeamsApi(mockRetrofit.create(TeamsApi::class.java))
@@ -38,5 +38,10 @@ class MockTeamsApi(val delegate: BehaviorDelegate<TeamsApi>) : TeamsApi {
 
     override fun stats(teamName: String?, accessToken: String?): Single<TeamStatsEntity>? {
         return delegate.returningResponse(stats).stats(teamName, accessToken)
+    }
+
+    override fun categories(teamName: String?, accessToken: String?): Observable<TeamsApi.Category>? {
+        // TODO: Not yet implemented.
+        throw UnsupportedOperationException()
     }
 }
